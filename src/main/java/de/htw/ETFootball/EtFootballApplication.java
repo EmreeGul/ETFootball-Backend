e3bf7eb9c64a23a3b2e9b263709c67481b80172d
Emre Tekgül <Emre.Tekguel@Student.HTW-Berlin.de>
2024-06-24 15:15:02 +0200

    Update 3

--- src/main/java/de/htw/ETFootball/EtFootballApplication.java
+++ src/main/java/de/htw/ETFootball/EtFootballApplication.java
@@ -2,7 +2,9 @@ package de.htw.ETFootball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class EtFootballApplication {
 
