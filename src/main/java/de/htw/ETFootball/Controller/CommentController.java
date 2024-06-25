c6d011828339478a69875cbefa132a0c3f9086db
Emre Tekgül <Emre.Tekguel@Student.HTW-Berlin.de>
2024-06-25 17:19:53 +0200

    Request Mapping

--- src/main/java/de/htw/ETFootball/Controller/CommentController.java
+++ src/main/java/de/htw/ETFootball/Controller/CommentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("https://etfootball-backend.onrender.com/")
 @CrossOrigin(origins = {"https://etfootball-frontend.onrender.com", "http://localhost:5173", "http://localhost:8080"})
 public class CommentController {
 
